5d7dba8791f3f875c125b1ac05f048fb58fdf87c
mahditogighi22 <mahdiartrapi@gmail.com>
2025-12-01 13:43:56 +0330

    Delete contact.php

--- contact.php
+++
@@ -1,5 +0,0 @@
-<?php include "includes/header.php"; ?>
-<h2>تماس با ما</h2>
-<p>برای ارتباط با ما از ایمیل <a href="mailto:info@vento.ir">info@vento.ir</a> استفاده کنید.</p>
-<?php include "includes/footer.php"; ?>
-
